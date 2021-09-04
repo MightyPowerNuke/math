760ba1b84bc7b8c876a8341c994e7441af1c6455
Soma Vargyas <vsomos@gmail.com>
2021-09-04 16:20:12 +0200

    Introduce use header

---
+++ math/use_math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "math.h"
+#include "matrix.h"
+#include "point.h"
+#include "polar.h"
+#include "primitives.h"
+#include "random.h"
+#include "spherical.h"
+#include "transform.h"
+#include "vector.h"
